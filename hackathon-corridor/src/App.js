dbcf7d42d9228b0e90d2c0469c24f341a8a712b7
Cation-Coder6 <200516@kiit.ac.in>
2023-03-25 12:06:51 +0530

    added pdf generator setup

--- hackathon-corridor/src/App.js
+++ hackathon-corridor/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import CsvToJson from './components/CsvToJson';
+import GeneratePDF from './components/pdfGenerator';
 
 function App() {
-  return (
-    <div className="App">
-      <CsvToJson />
-    </div>
-  );
+    return (
+        <div className="App">
+            <CsvToJson />
+            <GeneratePDF />
+        </div>
+    );
 }
 
 export default App;
