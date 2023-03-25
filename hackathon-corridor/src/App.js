0b1e8cf8479025f8ebc80c6fcae13b26f3c3d999
Sibananda Sahu <sibananda6481@gmail.com>
2023-03-25 12:07:54 +0530

    bar graph implemented

--- hackathon-corridor/src/App.js
+++ hackathon-corridor/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import CsvToJson from './components/CsvToJson';
+import Graphs from './components/Graphs/Graphs';
 
 function App() {
   return (
     <div className="App">
       <CsvToJson />
+      <Graphs />
     </div>
   );
 }
